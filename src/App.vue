1a006a8dca006f5c90fc13cbba788d6a5e563c6a
ZHAOSHUANGSHUANG <zhaoshuangshuang@xinguangnet.com>
2018-12-21 11:18:21 +0800

    路由动态效果

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <!-- <transition> -->
+      <router-view/>
+    <!-- </transition> -->
   </div>
 </template>
 
