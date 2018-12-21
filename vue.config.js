63c34b46d87ca2b4f850fa7df8023f57cbd28bcf
ZHAOSHUANGSHUANG <zhaoshuangshuang@xinguangnet.com>
2018-12-21 11:16:33 +0800

    vue webpack 新的配置放在vue.config.js文件中

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// vue.config.js
+module.exports = {
+    // webpack config
+    // 该对象将会被 webpack-merge 合并入最终的 webpack 配置。
+    configureWebpack: {
+        plugins: [
+            // new MyAwesomeWebpackPlugin()
+        ]
+    }
+}
